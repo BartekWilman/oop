f55c267d0ce1b81eeb3359d6fc2474ceb7d3d7d0
Bartłomiej Wilman <bartekwilman@op.pl>
2021-11-20 11:46:24 +0100

    Silnik działa i zgasł

---
+++ ObjcectPrograming/src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    public void startEngine(){
+        System.out.println("Silnik działa");
+    }
+        public void stopEngine(){
+            System.out.println("Silnik zgasł");
+
+    }
+}
